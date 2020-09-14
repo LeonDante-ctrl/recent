fdb810d8a0229493203d7200c7059ebe5b0437be
leondante-ctrl <maxgamerdu@gmail.com>
2020-09-14 04:00:30 +0300

    fixed indention errors in code

--- app.py
+++ app.py
@@ -9,16 +9,46 @@ def Index():
     newsapi = NewsApiClient(api_key="b2a3ef2c39444fb799e98debbcdd4257")
     topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
 
+    articles = topheadlines['articles']
 
-articles = topheadlines['articles']
+    desc = []
+    news = []
+    img = []
 
-desc = []
-news = []
-img = []
+    for i in range(len(articles)):
+        myarticles = articles[i]
 
-for i in range(len(articles)):
-    myarticles = articles[i]
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
 
-    news.append(myarticles['title'])
-    desc.append(myarticles['description'])
-    img.append(myarticles['urlToImage'])
+    mylist = zip(news, desc, img)
+
+    return render_template('index.html', context=mylist)
+
+
+@app.route('/bbc')
+def bbc():
+    newsapi = NewsApiClient(api_key="b2a3ef2c39444fb799e98debbcdd4257")
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+
+    mylist = zip(news, desc, img)
+
+    return render_template('bbcnews.html', context = mylist)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
