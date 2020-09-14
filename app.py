515224ec6517115a2076b475bc7d801d53c08902
leondante-ctrl <maxgamerdu@gmail.com>
2020-09-14 03:47:41 +0300

    added object that will appent my top headlines into the list

--- app.py
+++ app.py
@@ -8,3 +8,17 @@ app = Flask(__name__)
 def Index():
     newsapi = NewsApiClient(api_key="b2a3ef2c39444fb799e98debbcdd4257")
     topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
+
+
+articles = topheadlines['articles']
+
+desc = []
+news = []
+img = []
+
+for i in range(len(articles)):
+    myarticles = articles[i]
+
+    news.append(myarticles['title'])
+    desc.append(myarticles['description'])
+    img.append(myarticles['urlToImage'])
